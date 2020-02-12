45071c1cea317513a205d93b21ec3ee28f475959
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-02-12 15:06:03 -0600

    added form template and view

---
+++ hrapp/views/computers/computer_form.py
@@ -0,0 +1,33 @@
+import sqlite3
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from hrapp.models import Computer
+from ..connection import Connection
+
+
+def get_computers():
+    with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+                c.id,
+                c.make,
+                c.purchase_date,
+                c.decommission_date
+            from hrapp_computer c
+            """)
+
+        return db_cursor.fetchall()
+
+@login_required
+def computer_form(request):
+    if request.method == 'GET':
+        computers = get_computers()
+        template = 'computers/form.html'
+        context = {
+            'all_computers': computers
+        }
+
+        return render(request, template, context)
