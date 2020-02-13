153047343d5bc6448fe15f5b8024e787193e347e
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-02-12 19:40:01 -0600

    form there but broken

--- hrapp/views/computers/computer_list.py
+++ hrapp/views/computers/computer_list.py
@@ -1,5 +1,5 @@
 import sqlite3
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from hrapp.models import Computer
 from ..connection import Connection
 
@@ -18,7 +18,7 @@ def computer_list(request):
                 c.make,
                 c.purchase_date,
                 c.decommission_date
-            from hrapp_computer c
+                from hrapp_computer c
             """)
 
             all_computers = []
@@ -40,3 +40,21 @@ def computer_list(request):
     }
 
     return render(request, template, context)
+
+    if request.method == 'POST':
+        form_data = request.POST
+
+    with sqlite3.connect(Connection.db_path) as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO hrapp_computer
+        (
+            make, purchase_date, decommission_date
+        )
+        VALUES (?, ?, ?)
+        """,
+        (form_data['make'], form_data['purchase_date'],
+            form_data['decommission_date']))
+
+    return redirect(reverse('hrapp:computers'))
