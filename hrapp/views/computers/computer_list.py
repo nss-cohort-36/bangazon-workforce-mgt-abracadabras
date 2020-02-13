a8253f657d64d0a9382b75acfb8ca74059afb32e
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-02-12 19:56:38 -0600

    fixed the white spacing

--- hrapp/views/computers/computer_list.py
+++ hrapp/views/computers/computer_list.py
@@ -34,27 +34,27 @@ def computer_list(request):
 
                 all_computers.append(computer)
 
-    template = 'computers/computers_list.html'
-    context = {
-        'computers': all_computers
-    }
+        template = 'computers/computers_list.html'
+        context = {
+            'computers': all_computers
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         form_data = request.POST
 
-    with sqlite3.connect(Connection.db_path) as conn:
-        db_cursor = conn.cursor()
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
 
-        db_cursor.execute("""
-        INSERT INTO hrapp_computer
-        (
-            make, purchase_date, decommission_date
-        )
-        VALUES (?, ?, ?)
-        """,
-        (form_data['make'], form_data['purchase_date'],
-            form_data['decommission_date']))
+            db_cursor.execute("""
+            INSERT INTO hrapp_computer
+            (
+                make, purchase_date, decommission_date
+            )
+            VALUES (?, ?, ?)
+            """,
+            (form_data['make'], form_data['purchase_date'],
+                form_data['decommission_date']))
 
     return redirect(reverse('hrapp:computers'))
