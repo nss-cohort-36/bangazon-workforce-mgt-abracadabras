90e83b8f407a2f3c33e026bf74c7216c50fe84e5
rhc822 <rhc822@gmail.com>
2020-02-13 11:29:23 -0600

    working on new_employee_form

--- hrapp/views/employees/new_employee_form.py
+++ hrapp/views/employees/new_employee_form.py
@@ -1,47 +1,52 @@
 import sqlite3
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from libraryapp.models import Book
-from libraryapp.models import Library
+# from libraryapp.models import Book
+# from libraryapp.models import Library
 # from libraryapp.models import model_factory
 from ..connection import Connection
 
 
-def get_libraries():
+def get_employees():
     with sqlite3.connect(Connection.db_path) as conn:
         conn.row_factory = sqlite3.Row
-        # This is different than chapter 5, because I didn't add the "conn.row_factory = model_factory(Library)" that was in chapter 3, "Advanced Creation of Simple Model Instances" section"
-
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
         select
-            l.id,
-            l.title,
-            l.address
-        from libraryapp_library l
+            emp.id,
+            emp.first_name,
+            emp.last_name,
+            emp.start_date,
+            emp.is_supervisor,
+            emp.department_id
+        from hrapp_employee emp
         """)
 
-        all_libraries = []
+        all_employees = []
         dataset = db_cursor.fetchall()
 
         for row in dataset:
-            library = Library()
-            library.id = row["id"]
-            library.title = row["title"]
-            library.address = row["address"]
+            employee = Employee()
+            employee.id = row["id"]
+            employee.first_name = row["first_name"]
+            employee.last_name = row["last_name"]
+            employee.start_date = row["start_date"]
+            employee.is_supervisor = row["is_supervisor"]
+            employee.department_id = row["department_id"]
 
-            all_libraries.append(library)
+            all_employees.append(employee)
 
-        return all_libraries
+        return all_employees
 
-@login_required
-def book_form(request):
-    if request.method == 'GET':
-        libraries = get_libraries()
-        template = 'books/form.html'
-        context = {
-            'all_libraries': libraries
-        }
 
-        return render(request, template, context)
+# @login_required
+# def add_employee_form(request):
+#     if request.method == 'GET':
+#         libraries = get_libraries()
+#         template = 'books/form.html'
+#         context = {
+#             'all_libraries': libraries
+#         }
+
+#         return render(request, template, context)
